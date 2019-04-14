08bb17f11166f29d4d8c82c82fc6e449c09c1298
mosesren <renhui774411@163.com>
2019-04-14 22:54:34 +0800

    first commit add six solution of leetcode implement by java

---
+++ src/com/jehu/_006 ZigZag Conversion/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        int len =2*numRows-2; 
+        int [] a = new int [len];
+        for(int i=0;i<numRows;i++){
+            a[i] = i;
+        }
+        for(int j=1;j+numRows-1<len;j++){
+            a[j+numRows-1]=a[numRows-1-j];
+        }
+        StringBuffer[] sbBuffers = new StringBuffer[numRows];
+        for(int i=0;i<numRows;i++){
+            sbBuffers[i] = new StringBuffer();
+        }
+        for(int i = 0;i<s.length();i++){
+            int flag = a[i%len];
+            sbBuffers[flag].append(s.charAt(i));
+        }
+        String result = "";
+        for(StringBuffer ss:sbBuffers){
+            result+=ss.toString();
+        }
+        return result;
+    }
+}
