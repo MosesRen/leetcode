1e5036ca79b6b63c0301027f67eb05ec46771306
mosesren <renhui774411@163.com>
2019-04-15 11:02:49 +0800

    add the solution of 007 leetcode

---
+++ src/com/jehu/_007 Reverse Integer/Solution.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
+class Solution {
+    public int reverse(int x) {
+        if(x==0){
+            return x;
+        }
+        boolean flag =(x>0)?true:false;
+        x = (flag)?x:-x;
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuffer stringBuffer = new StringBuffer();
+        while(x>0){
+            queue.add(x%10);
+            x = x/10;
+        }
+        while(queue.peek()!=null){
+            stringBuffer.append(queue.poll());
+        }
+        int result = 0;
+        try{
+            result = Integer.parseInt(stringBuffer.toString());
+        }catch(Exception e){
+            return 0;
+        }
+        return (flag)?result:result*(-1);
+    }
+    public static void main(String[] args) {
+        int x =1534236469;
+        Solution su = new Solution();
+        int y = su.reverse(x);
+        System.out.println(y);
+        
+    }
+}
