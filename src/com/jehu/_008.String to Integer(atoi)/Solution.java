85588eac0e19f3b0a298c11b438c3aab0c7dbe94
mosesren <renhui774411@163.com>
2019-04-16 14:44:39 +0800

    add  8 solution

---
+++ src/com/jehu/_008.String to Integer(atoi)/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Solution{
+    public int myAtoi(String str){
+        String test = str.trim();
+        int a = 0;
+        String result ="";
+        Pattern pattern = Pattern.compile("^[-|\\+]?[\\d]+");
+        Matcher matcher = pattern.matcher(test);
+        if(matcher.find()){
+            result = matcher.group();
+            if(result.length()!=0){
+                try{
+                    a = Integer.parseInt(result);
+                }catch(Exception e){
+                    if(result.charAt(0)=='-'){
+                        a = -2147483648;
+                    }else{
+                        a = 2147483647;
+                    }
+                }
+            }
+
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Solution solution = new Solution();
+        System.out.println(solution.myAtoi(str));
+        sc.close();
+    }
+}
